720372a82f2f27ad14aa2769e5ce546e5a7ef00f
Abdullah Masood <bscs22054@itu.edu.pk>
2024-10-22 10:45:23 +0500

    Add all components , accidently submitted it in assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home'; 
+import Footer from './Components/Footer';
 
 function App() {
+  const [category, setCategory] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar setCategory={setCategory} />
+      <Home category={category} />
+      <Footer/>
+    </Router>
   );
 }
 
