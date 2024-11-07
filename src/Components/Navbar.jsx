dcb67fe7d34596f4398fdc5af78d541e5ae8efe3
Abdullah Masood <bscs22054@itu.edu.pk>
2024-11-07 23:22:03 +0500

    Added Icon for logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -32,81 +32,86 @@ const Navbar = ({ setCategory }) => {
     }, []);
 
     return (
-        <header className="navbar-header">
-            <div className="navbar-wrapper">
-                <div className="navbar-inner">
-                    <div className="flex-shrink-0">
-                        <NavLink to="/" className="navbar-logo">
-                            Airbnb
-                        </NavLink>
-                    </div>
-
-                    {showSearchBar ? (
-                        <div className="navbar-searchbar visible">
-                            <p className="searchbar-input">Anywhere</p>
-                            <p className="searchbar-input">Any week</p>
-                            <p className="searchbar-input placeholder">Any Guests</p>
-                            <AiOutlineSearch className="searchbar-icon" />
-                        </div>
-                    ) : (
-                        <div className="navbar-links visible">
-                            <NavLink to="/stays" className="nav-link">
-                                Stays
-                            </NavLink>
-                            <NavLink to="/experiences" className="nav-link">
-                                Experiences
-                            </NavLink>
-                        </div>
-                    )}
-
-                    {/* Right Side Icons */}
-                    <div className="navbar-right"> 
-                        <button className="right-link">
-                            <IoMdGlobe className="navbar-icon" />
-                        </button>
-                        <div onClick={toggleMenu} className="navbar-icons">
-                            <div className="sm:block hidden">
-                                <GiHamburgerMenu className="navbar-icon" />
-                            </div>
-                            <div className="sm:hidden">
-                                <button onClick={toggleMenu}>
-                                    {isOpen ? <FaTimes className="navbar-icon" /> : <FaBars className="navbar-icon" />}
-                                </button>
-                            </div>
-                            <FaUserCircle className="navbar-profile" />
-                        </div>
-                    </div>
-                </div>
-
-                {/* Mobile Menu */}
-                {isOpen && (
-                    <div className="mobile-menu visible">
-                        <NavLink to="/" className="mobile-menu-link">
-                            Sign up
-                        </NavLink>
-                        <NavLink to="/" className="mobile-menu-link">
-                            Log in
-                        </NavLink>
-                        <div className="mobile-menu-divider"></div>
-                        <NavLink to="/" className="mobile-menu-link">
-                            Gift Cards
-                        </NavLink>
-                        <NavLink to="/" className="mobile-menu-link">
-                            Airbnb Your Home
-                        </NavLink>
-                        <NavLink to="/" className="mobile-menu-link">
-                            Host an Experience
-                        </NavLink>
-                        <NavLink to="/" className="mobile-menu-link">
-                            Help Center
-                        </NavLink>
-                    </div>
-                )}
+      <header className="navbar-header">
+        <div className="navbar-wrapper">
+          <div className="navbar-inner">
+            <div className="flex-shrink-0">
+              <NavLink to="/" className="navbar-logo">
+                <img src="/Airbnb.ico" alt="Airbnb" className="image" />
+                Airbnb
+              </NavLink>
             </div>
 
-            <SearchBar />
-            <HorizontalScrollList setCategory={setCategory} />
-        </header>
+            {showSearchBar ? (
+              <div className="navbar-searchbar visible">
+                <p className="searchbar-input">Anywhere</p>
+                <p className="searchbar-input">Any week</p>
+                <p className="searchbar-input placeholder">Any Guests</p>
+                <AiOutlineSearch className="searchbar-icon" />
+              </div>
+            ) : (
+              <div className="navbar-links visible">
+                <NavLink to="/stays" className="nav-link">
+                  Stays
+                </NavLink>
+                <NavLink to="/experiences" className="nav-link">
+                  Experiences
+                </NavLink>
+              </div>
+            )}
+
+            {/* Right Side Icons */}
+            <div className="navbar-right">
+              <button className="right-link">
+                <IoMdGlobe className="navbar-icon" />
+              </button>
+              <div onClick={toggleMenu} className="navbar-icons">
+                <div className="sm:block hidden">
+                  <GiHamburgerMenu className="navbar-icon" />
+                </div>
+                <div className="sm:hidden">
+                  <button onClick={toggleMenu}>
+                    {isOpen ? (
+                      <FaTimes className="navbar-icon" />
+                    ) : (
+                      <FaBars className="navbar-icon" />
+                    )}
+                  </button>
+                </div>
+                <FaUserCircle className="navbar-profile" />
+              </div>
+            </div>
+          </div>
+
+          {/* Mobile Menu */}
+          {isOpen && (
+            <div className="mobile-menu visible">
+              <NavLink to="/" className="mobile-menu-link">
+                Sign up
+              </NavLink>
+              <NavLink to="/" className="mobile-menu-link">
+                Log in
+              </NavLink>
+              <div className="mobile-menu-divider"></div>
+              <NavLink to="/" className="mobile-menu-link">
+                Gift Cards
+              </NavLink>
+              <NavLink to="/" className="mobile-menu-link">
+                Airbnb Your Home
+              </NavLink>
+              <NavLink to="/" className="mobile-menu-link">
+                Host an Experience
+              </NavLink>
+              <NavLink to="/" className="mobile-menu-link">
+                Help Center
+              </NavLink>
+            </div>
+          )}
+        </div>
+
+        <SearchBar />
+        <HorizontalScrollList setCategory={setCategory} />
+      </header>
     );
 };
 
