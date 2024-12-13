ebad0158b01210e93f687387c8d5750d977eb0e2
Abdullah Masood <bscs22054@itu.edu.pk>
2024-12-13 23:14:13 +0500

    Modify signIn design for image

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import "../Styles/SignIn.css";
+
 const SignIn = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [message, setMessage] = useState("");
@@ -15,9 +16,7 @@ const SignIn = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `${
-            import.meta.env.VITE_API_BASE_URL
-          }/api/auth/login`,
+        `${import.meta.env.VITE_API_BASE_URL}/api/auth/login`,
         formData
       );
 
@@ -42,33 +41,41 @@ const SignIn = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="signin-form">
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        onChange={handleChange}
-        value={formData.email}
-        required
-        className="signin-input"
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={handleChange}
-        value={formData.password}
-        required
-        className="signin-input"
-      />
-      <button type="submit" className="signin-button">
-        Sign In
-      </button>
-      <p className="signin-message">{message}</p>
-      <Link to="/signup" className="signup-link">
-        Sign Up
-      </Link>
-    </form>
+    <div className="signin-container">
+      <div className="signin-image">
+        <img src="./SignIn.jpeg" alt="SignIn Illustration" />
+      </div>
+
+      <div className="signin-form-container">
+        <form onSubmit={handleSubmit} className="signin-form">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            value={formData.email}
+            required
+            className="signin-input"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            value={formData.password}
+            required
+            className="signin-input"
+          />
+          <button type="submit" className="signin-button">
+            Sign In
+          </button>
+          <p className="signin-message">{message}</p>
+          <Link to="/signup" className="signup-link">
+            Sign Up
+          </Link>
+        </form>
+      </div>
+    </div>
   );
 };
 
